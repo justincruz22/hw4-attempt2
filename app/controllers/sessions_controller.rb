4f231aeb480300c828eadc74f88b3ecf6a98033f
justincruz22 <justin.cruz1@kellogg.northwestern.edu>
2022-03-14 10:32:36 +0000

    attempt 1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,27 @@ class SessionsController < ApplicationController
   end
 
   def create
+    entered_email = params["email"]
+    entered_password = params["password"]
+    @user = User.find_by({email: entered_email})
+    if @user
+      if BCrypt::Password.new(@user.password) == entered_password
+        session["user_id"] = @user.id
+        redirect_to "/"
+      else
+        flash[:notice] = "Your email and password combination does not match an account in our records. Please try again."
+        redirect_to "/sessions/new"
+      end
+    else
+      flash[:notice] = "Your email and password combination does not match an account in our records. Please try again."
+      redirect_to "/sessions/new"
+    end
   end
 
   def destroy
+    session["user_id"] = nil
+    flash[:notice] = "You have been logged out!" 
+    redirect_to "/sessions/new"
   end
 end
   
